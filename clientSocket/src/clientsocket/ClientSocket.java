c8373502f276c4587aface47a54009508e8be14c
manduca.fabio <manduca.fabio@TAVE-PC18.OSDBSESTO.local>
2021-03-04 14:00:31 +0100

    es nuovo

---
+++ clientSocket/src/clientsocket/ClientSocket.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clientsocket;
+
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author manduca.fabio
+ */
+public class ClientSocket {
+
+    /**
+     * @param args the command line arguments
+     */
+     public static void main(String[] args) {
+         try {
+             System.out.println("Apertua connessione");
+             
+             Socket server = new Socket("127.0.0.1", 5500);
+             
+             PrintWriter out
+                     = new PrintWriter(server.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(
+                     new InputStreamReader(server.getInputStream()));
+             Scanner tastiera = new Scanner(System.in);
+             String s = "";
+             while (!s.equals("exit")) {
+                 System.out.println("Inserire frase da inviare al server:");
+                 s = tastiera.nextLine();
+                 System.out.println("frase inserita: " + s);
+                 out.println(s);
+                 String risposta = in.readLine();
+                 System.out.println("risposta del server: " + risposta);
+                 System.out.println("hai inserito "+(risposta.split(": ")[1])+" caratteri");
+             }
+             
+             in.close();
+             server.close();
+             System.out.println("chiusura connessione");
+         } catch (IOException ex) {
+             Logger.getLogger(ClientSocket.class.getName()).log(Level.SEVERE, null, ex);
+         }
+    
+}
+}    
